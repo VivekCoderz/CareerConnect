3decc4fd12ba340e0366879b06cb2a19ae9215de
Vivek Garg <vivekgarg0605@gmail.com>
2026-09-11 13:38:31 +0530

    Update cloudinary.js

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -2,9 +2,9 @@ require("dotenv").config();
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME || process.env.Cloudinary_Cloud_Name || "dqe3aiebn",
-  api_key: process.env.CLOUDINARY_API_KEY || process.env.Cloudinary_API_Key || "929622791574745",
-  api_secret: process.env.CLOUDINARY_API_SECRET || process.env.Cloudinary_API_Secret || "Fq3vgRwDpemBB0tWSlFPLMDLOYk",
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME || process.env.Cloudinary_Cloud_Name,
+  api_key: process.env.CLOUDINARY_API_KEY || process.env.Cloudinary_API_Key,
+  api_secret: process.env.CLOUDINARY_API_SECRET || process.env.Cloudinary_API_Secret,
 });
 
 /**
